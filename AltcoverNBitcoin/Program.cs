c80db07492b528754f6c28683ba907a1c74e7448
nopara73 <adam.ficsor73@gmail.com>
2018-05-04 13:41:07 +0700

    Add project files.

---
+++ AltcoverNBitcoin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AltcoverNBitcoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
